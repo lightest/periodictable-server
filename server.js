32cea6fa6f0a8210984d9abfa9b7a74c857de4d1
lightest <lightest13@gmail.com>
2024-09-02 00:03:41 +0100

    experimenting with llm prompting for chemistry

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ import http from "http";
 import ollama from "ollama";
 
 const port = 3000;
-const MODEL = "gemma2";
+const MODEL = "gemma2"; // Bset so far.
+// const MODEL = "mistral-nemo";
+
+const PROMPTS = {
+    bondChemicals: "Conduct chemical reaction using given chemicals and atoms. Return resulting chemical with formula, name and amount of atoms for each element. Use only given atoms. Use format {formula: 'formula', name: 'name_of_chemical', elements: [{symbol, name, atoms}]}. If there are more than one chemical as a result of the chemical reaction, return all of them in the array. Return response directly, do not use markdown. Elements:"
+};
 
 const API_POST_HANDLERS = {
     "/api/chat": async (parsedBody, req, res) =>
@@ -20,9 +25,13 @@ const API_POST_HANDLERS = {
     "/api/bondElements": async (parsedBody, req, res) =>
     {
         console.log(parsedBody);
+        let elements = [];
+        for (let i = 0; i < parsedBody.elements.length; i++)
+        {
+            elements.push(JSON.stringify(parsedBody.elements[i]));
+        }
 
-        const llmMessage =
-            `Can these atoms of chemical elements given by JSON form a bond? Return resulting chemical element in the same format. Do not add atoms. No coding just object as string. ${JSON.stringify(parsedBody.el1)}, ${JSON.stringify(parsedBody.el2)}`;
+        const llmMessage = `${PROMPTS.bondChemicals} ${elements.join(", ")}`;
         console.log(llmMessage);
 
         const response = await ollama.generate({
@@ -78,7 +87,7 @@ const server = http.createServer(async (req, res) =>
                 res.writeHead(200, { "Content-Type": "application/json" });
                 if (response.response && typeof response.response === "string")
                 {
-                    res.end(JSON.stringify({ response: response.message }));
+                    res.end(JSON.stringify({ response: response.response }));
                 }
                 // res.end(JSON.stringify({ response: response.message }));
             }
